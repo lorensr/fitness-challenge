969c9877fb8d51f115562442869a6e4715c6fc29
Loren Sands-Ramshaw <lorensr@gmail.com>
2018-02-20 21:33:37 -0500

    Slack challenge worked, MONGO_URL env var worked

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const server = express()
+const MongoClient = require('mongodb').MongoClient
 
-server.get('/', (req, res) => res.send('Hello World!'))
+const DEV_MONGO_URL = 'mongodb://localhost:27017'
+
+const mongoUrl = process.env.MONGO_URL || DEV_MONGO_URL
+
+console.log('Connecting to: ', mongoUrl)
+
+// MongoClient.connect(url, function(err, client) {
+//   // Use the admin database for the operation
+//   const adminDb = client.db(dbName).admin();
+//   // List all the available databases
+//   adminDb.listDatabases(function(err, dbs) {
+//     test.equal(null, err);
+//     test.ok(dbs.databases.length > 0);
+//     client.close();
+//   });
+// });
+
+server.use(bodyParser.json())
+
+server.get('/', (req, res) => res.send('Hello hi!'))
+
+server.post('/slack', (req, res) => {
+  const message = req.body.event.text
+
+  res.send(req.body.challenge)
+})
 
 server.listen(3000, () => console.log('Example server listening on port 3000!'))
