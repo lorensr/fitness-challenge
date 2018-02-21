45c60a8c4554ac499cff7ded69a1669e4f3d07ff
Loren Sands-Ramshaw <lorensr@gmail.com>
2018-02-20 20:37:55 -0500

    Express is working

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const server = express()
+
+server.get('/', (req, res) => res.send('Hello World!'))
+
+server.listen(3000, () => console.log('Example server listening on port 3000!'))
